d7be773dbc8545440be1f513e5cfc9c741c2dfaa
Bonus <root@bonusplay.pl>
2022-11-28 22:39:04 +0100

    kaldir: hotfix networking

--- machines/kaldir/networking.nix
+++ machines/kaldir/networking.nix
@@ -1,6 +1,7 @@
 {
   networking = {
-    hostName = "kaldir.bonus.p4";
+    hostName = "kaldir";
+    domain = "bonus.p4";
     nameservers = [ "1.1.1.1" "8.8.8.8" ];
     firewall = {
       allowedTCPPorts = [ 13337 ];
