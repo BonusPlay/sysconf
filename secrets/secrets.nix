d0a0d09535333001c301f0dcb9512e9f8e358137
Bonus <root@bonusplay.pl>
2022-11-29 19:57:51 +0100

    secrets: rekey for kaldir

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -3,7 +3,7 @@ let
   users = [ bonus ];
 
   artanis = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFDM0mEeN9Z7TRf0cnx0Gpkv8at2tl0++Sr1MmxpWIZn";
-  kaldir = "";
+  kaldir = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICRWkpRKUp7gfqT6Lu8e46klnAwdIz4SpEIQTvZ2pyBx";
   systems = [ artanis kaldir ];
 in
 {
