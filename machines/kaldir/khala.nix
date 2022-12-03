f60ac29ca29bb69f46526fa801d2140b7382fe7f
Bonus <root@bonusplay.pl>
2022-12-03 13:44:39 +0100

    kaldir: move khala port

--- machines/kaldir/khala.nix
+++ machines/kaldir/khala.nix
@@ -23,7 +23,7 @@
   services.headscale = {
     enable = true;
     address = "0.0.0.0";
-    port = 8080;
+    port = 51850;
     serverUrl = "https://kaldir.bonusplay.pl";
     tls.keyFile = "/var/lib/acme/wildcard/key.pem";
     tls.certFile = "/var/lib/acme/wildcard/cert.pem";
