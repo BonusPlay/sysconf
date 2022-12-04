f3fda835ef2eec4e136eba747554fe04c52647f4
Bonus <root@bonusplay.pl>
2022-12-04 22:53:05 +0100

    kaldir: test headscale acls

--- machines/kaldir/khala.nix
+++ machines/kaldir/khala.nix
@@ -27,6 +27,7 @@
     serverUrl = "https://kaldir.bonusplay.pl";
     tls.keyFile = "/var/lib/acme/wildcard/key.pem";
     tls.certFile = "/var/lib/acme/wildcard/cert.pem";
+    aclPolicyFile = "/etc/headscale/acl.json";
     settings = {
       ip_prefixes = "198.18.66.0/24";
       tls_client_auth_mode = "disabled";
@@ -38,5 +39,33 @@
   services.tailscale = {
     enable = true;
     interfaceName = "p4net-khala";
+  };
+
+  environment.etc."headscale/acl.json" = let
+    aclConfig = {
+      groups = {
+        "group:bonus" = [ "bonus" ];
+        "group:servers" = [ "kaldir" ];
+      };
+      tagOwners = {
+        "tag:servers" = [ "group:servers" ];
+      };
+      acls = [
+        {
+          action = "accept";
+          src = [ "bonus" ];
+          dst = [ "bonus:*" ];
+        }
+        {
+          action = "accept";
+          src = [ "group:bonus" ];
+          dst = [ "tag:servers:*" ];
+        }
+      ];
+    };
+  in {
+    user = "headscale";
+    mode = "0660";
+    text = builtins.toJSON aclConfig;
   };
 }
