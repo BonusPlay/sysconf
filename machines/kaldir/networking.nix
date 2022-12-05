0d6d0ec45d48564b4a1647b4ca25e12975c953ac
Bonus <root@bonusplay.pl>
2022-12-05 23:42:40 +0100

    kaldir: add policy routing for khala

--- machines/kaldir/networking.nix
+++ machines/kaldir/networking.nix
@@ -11,9 +11,24 @@
     iproute2 = {
       enable = true;
       rttablesExtraConfig = ''
-        1 hive
+        1 khala
       '';
     };
+
+    systemd.services.khalaPolicyRouting = {
+      wantedBy = [ "network-online.target" ];
+      after = "network-interfaces.target";
+      description = "Add policy routing for khala interface";
+      script = ''
+        ${pkgs.iproute2}/bin/ip rule add from 10.0.2.10 lookup khala
+        ${pkgs.iproute2}/bin/ip rule add from 10.0.2.1 lookup khala
+      '';
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+      };
+    };
+
     interfaces.enp0s3 = {
       useDHCP = false;
       ipv4 = {
@@ -28,6 +43,7 @@
         }];
       };
     };
+
     interfaces.enp1s0 = {
       useDHCP = false;
       ipv4 = {
@@ -35,14 +51,23 @@
           address = "10.0.2.10";
           prefixLength = 24;
         }];
-        routes = [{
-          address = "0.0.0.0";
-          prefixLength = 0;
-          via = "10.0.2.1";
-          options = {
-            table = "hive";
-          };
-        }];
+        routes = [
+          {
+            address = "10.0.2.0";
+            prefixLength = 24;
+            options = {
+              table = "khala";
+            };
+          }
+          {
+            address = "0.0.0.0";
+            prefixLength = 0;
+            via = "10.0.2.1";
+            options = {
+              table = "khala";
+            };
+          }
+        ];
       };
     };
   };
