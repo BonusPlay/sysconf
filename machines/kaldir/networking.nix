878f880a641a1689ba0f239b4811b5bc6cd429a5
Bonus <root@bonusplay.pl>
2022-12-09 14:03:01 +0100

    kaldir: hotfix networking

--- machines/kaldir/networking.nix
+++ machines/kaldir/networking.nix
@@ -9,18 +9,18 @@
       allowedUDPPorts = [ 13337 ];
     };
     dhcpcd.enable = false;
-    iproute2 = {
-      enable = true;
-      rttablesExtraConfig = ''
-        1 khala
-      '';
-    };
+    #iproute2 = {
+    #  enable = true;
+    #  rttablesExtraConfig = ''
+    #    1 khala
+    #  '';
+    #};
 
     interfaces.enp0s3 = {
       useDHCP = false;
       ipv4 = {
         addresses = [{
-          address = "10.0.0.49";
+          address = "10.0.0.131";
           prefixLength = 24;
         }];
         routes = [{
@@ -31,45 +31,45 @@
       };
     };
 
-    interfaces.enp1s0 = {
-      useDHCP = false;
-      ipv4 = {
-        addresses = [{
-          address = "10.0.2.10";
-          prefixLength = 24;
-        }];
-        routes = [
-          {
-            address = "10.0.2.0";
-            prefixLength = 24;
-            options = {
-              table = "khala";
-            };
-          }
-          {
-            address = "0.0.0.0";
-            prefixLength = 0;
-            via = "10.0.2.1";
-            options = {
-              table = "khala";
-            };
-          }
-        ];
-      };
-    };
+    #interfaces.enp1s0 = {
+    #  useDHCP = false;
+    #  ipv4 = {
+    #    addresses = [{
+    #      address = "10.0.2.10";
+    #      prefixLength = 24;
+    #    }];
+    #    routes = [
+    #      {
+    #        address = "10.0.2.0";
+    #        prefixLength = 24;
+    #        options = {
+    #          table = "khala";
+    #        };
+    #      }
+    #      {
+    #        address = "0.0.0.0";
+    #        prefixLength = 0;
+    #        via = "10.0.2.1";
+    #        options = {
+    #          table = "khala";
+    #        };
+    #      }
+    #    ];
+    #  };
+    #};
   };
 
-  systemd.services.khalaPolicyRouting = {
-    wantedBy = [ "network-online.target" ];
-    after = [ "network-interfaces.target" ];
-    description = "Add policy routing for khala interface";
-    script = ''
-      ${pkgs.iproute2}/bin/ip rule add from 10.0.2.10 lookup khala
-      ${pkgs.iproute2}/bin/ip rule add from 10.0.2.1 lookup khala
-    '';
-    serviceConfig = {
-      Type = "oneshot";
-      RemainAfterExit = true;
-    };
-  };
+  #systemd.services.khalaPolicyRouting = {
+  #  wantedBy = [ "network-online.target" ];
+  #  after = [ "network-interfaces.target" ];
+  #  description = "Add policy routing for khala interface";
+  #  script = ''
+  #    ${pkgs.iproute2}/bin/ip rule add from 10.0.2.10 lookup khala
+  #    ${pkgs.iproute2}/bin/ip rule add from 10.0.2.1 lookup khala
+  #  '';
+  #  serviceConfig = {
+  #    Type = "oneshot";
+  #    RemainAfterExit = true;
+  #  };
+  #};
 }
