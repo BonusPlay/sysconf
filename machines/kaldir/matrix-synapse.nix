9881fdbc4acb92c4b8a107034e0bf22704998ec1
Bonus <root@bonusplay.pl>
2022-12-10 17:43:32 +0100

    kaldir: hotfix postgres init script

--- machines/kaldir/matrix-synapse.nix
+++ machines/kaldir/matrix-synapse.nix
@@ -41,7 +41,7 @@
   services.postgresql = {
     enable = true;
     initialScript = pkgs.writeText "synapse-init.sql" ''
-      CREATE ROLE "matrix-synapse";
+      CREATE ROLE "matrix-synapse WITH LOGIN";
       CREATE DATABASE "matrix-synapse" WITH OWNER "matrix-synapse"
         TEMPLATE template0
         LC_COLLATE = "C"
