4a03bb93cfbc65ff4ee9dafd79b2751c2c3724d5
Bonus <root@bonusplay.pl>
2023-01-10 23:40:48 +0100

    kaldir: prom stuff

--- machines/kaldir/prometheus.nix
+++ machines/kaldir/prometheus.nix
@@ -26,21 +26,17 @@
     exporters = {
       node = {
         enable = true;
-        listenAddress = "127.0.0.1";
       };
       bird = {
         enable = true;
-        listenAddress = "127.0.0.1";
         birdVersion = 2;
       };
       systemd = {
         enable = true;
-        listenAddress = "127.0.0.1";
       };
       wireguard = {
         enable = true;
         withRemoteIp = true;
-        listenAddress = "127.0.0.1";
       };
     };
   };
