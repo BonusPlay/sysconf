e669cd7a26c0404df608239fb4550cc85ffc1335
Bonus <root@bonusplay.pl>
2023-01-11 22:24:32 +0100

    artanis: hopefully autoconfigure p4net

--- machines/artanis/p4net.nix
+++ machines/artanis/p4net.nix
@@ -12,14 +12,14 @@
   '';
 
   systemd.network = {
-    networks."50-zerotier" = {
-      name = "p4net";
-      dns = [ "198.18.66.1" ];
-      domains = [ "p4" ];
-      networkConfig = {
-        DNSSEC = false;
-      };
-    };
+    #networks."50-zerotier" = {
+    #  name = "p4net";
+    #  dns = [ "198.18.66.1" ];
+    #  domains = [ "p4" ];
+    #  networkConfig = {
+    #    DNSSEC = false;
+    #  };
+    #};
     wait-online.ignoredInterfaces = [ "p4net" ];
   };
 }
