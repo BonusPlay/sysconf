50539ec628671711392fc8392cb0f7e15599d4fd
Bonus <root@bonusplay.pl>
2023-01-11 22:25:01 +0100

    kaldir: migrate to networkd, yolooooo

--- machines/kaldir/networking.nix
+++ machines/kaldir/networking.nix
@@ -3,26 +3,18 @@
   networking = {
     hostName = "kaldir";
     domain = "bonus.p4";
+    useNetworkd = true;
+    dhcpcd.enable = false;
     nameservers = [ "1.1.1.1" "8.8.8.8" ];
     firewall = {
       allowedTCPPorts = [ 13337 ];
       allowedUDPPorts = [ 13337 ];
-      checkReversePath = "loose";
     };
-    dhcpcd.enable = false;
-    #iproute2 = {
-    #  enable = true;
-    #  rttablesExtraConfig = ''
-    #    1 khala
-    #  '';
-    #};
-
     nat = {
       enable = true;
       externalInterface = "enp0s3";
       internalInterfaces = ["ve-+"];
     };
-
     interfaces.enp0s3 = {
       useDHCP = false;
       ipv4 = {
@@ -37,46 +29,5 @@
         }];
       };
     };
-
-    #interfaces.enp1s0 = {
-    #  useDHCP = false;
-    #  ipv4 = {
-    #    addresses = [{
-    #      address = "10.0.2.10";
-    #      prefixLength = 24;
-    #    }];
-    #    routes = [
-    #      {
-    #        address = "10.0.2.0";
-    #        prefixLength = 24;
-    #        options = {
-    #          table = "khala";
-    #        };
-    #      }
-    #      {
-    #        address = "0.0.0.0";
-    #        prefixLength = 0;
-    #        via = "10.0.2.1";
-    #        options = {
-    #          table = "khala";
-    #        };
-    #      }
-    #    ];
-    #  };
-    #};
   };
-
-  #systemd.services.khalaPolicyRouting = {
-  #  wantedBy = [ "network-online.target" ];
-  #  after = [ "network-interfaces.target" ];
-  #  description = "Add policy routing for khala interface";
-  #  script = ''
-  #    ${pkgs.iproute2}/bin/ip rule add from 10.0.2.10 lookup khala
-  #    ${pkgs.iproute2}/bin/ip rule add from 10.0.2.1 lookup khala
-  #  '';
-  #  serviceConfig = {
-  #    Type = "oneshot";
-  #    RemainAfterExit = true;
-  #  };
-  #};
 }
