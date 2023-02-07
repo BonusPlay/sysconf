dcddab379793c9e2022761f0b230a8932678ef09
Bonus <root@bonusplay.pl>
2023-02-08 00:27:47 +0100

    braxis: use dhcp

--- machines/braxis/networking.nix
+++ machines/braxis/networking.nix
@@ -2,29 +2,13 @@
 {
   networking = {
     hostName = "braxis";
-    domain = "bonus.p4";
     nameservers = [ "1.1.1.1" "8.8.8.8" ];
-    dhcpcd.enable = false;
+    useDHCP = true;
 
     firewall = {
       enable = true;
       allowedTCPPorts = [ 13337 ];
       allowedUDPPorts = [ 13337 ];
-    };
-
-    interfaces.ens18 = {
-      useDHCP = false;
-      ipv4 = {
-        addresses = [{
-          address = "10.0.0.30";
-          prefixLength = 24;
-        }];
-        routes = [{
-          address = "0.0.0.0";
-          prefixLength = 0;
-          via = "10.0.0.1";
-        }];
-      };
     };
   };
 }
