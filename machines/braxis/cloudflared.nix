b0a5ae111135f7d82ce9818443d696a0504b842e
Bonus <root@bonusplay.pl>
2023-03-20 23:54:41 +0100

    braxis: fix cloudflared

--- machines/braxis/cloudflared.nix
+++ machines/braxis/cloudflared.nix
@@ -1,7 +1,13 @@
 { config, ... }:
 {
-  age.secrets.tunnelPass = {
+  age.secrets.keycloakPass = {
     file = ../../secrets/keycloak-tunnel.age;
+    mode = "0440";
+    group = "cloudflared";
+  };
+
+  age.secrets.hedgedocPass = {
+    file = ../../secrets/hedgedoc-tunnel.age;
     mode = "0440";
     group = "cloudflared";
   };
@@ -10,7 +16,7 @@
     enable = true;
     tunnels = {
       "25ffc582-8ac1-48a6-b519-093e76104d54" = {
-        credentialsFile = config.age.secrets.tunnelPass.path;
+        credentialsFile = config.age.secrets.keycloakPass.path;
         default = "http_status:404";
         ingress = {
           "keycloak.kncyber.pl" = {
@@ -19,7 +25,7 @@
         };
       };
       "3ebda45f-5c0b-4d17-a6fb-6f1d7d3f1d4c" = {
-        credentialsFile = config.age.secrets.tunnelPass.path;
+        credentialsFile = config.age.secrets.hedgedocPass.path;
         default = "http_status:404";
         ingress = {
           "md.kncyber.pl" = {
