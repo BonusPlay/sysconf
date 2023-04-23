5eccef6bed5f6db4548ff4439e7668041e3cd727
Bonus <root@bonusplay.pl>
2023-04-23 12:12:59 +0200

    artanis: fix zerotier setup

--- machines/artanis/zerotier.nix
+++ machines/artanis/zerotier.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, system, pkgs, ... }:
 {
   security.pki.certificates = [ (builtins.readFile ../../files/p4net/p4net-ca.crt) ];
 
@@ -7,20 +7,20 @@
     joinNetworks = [ "b15644912e097589" "93afae59633bb8b8" ];
   };
 
+  systemd.services.zerotierone.postStart = let
+    resolvectl = "${pkgs.systemd}/bin/resolvectl";
+  in ''
+    ${resolvectl} dns p4net 198.18.66.1
+    ${resolvectl} domain p4net 'p4'
+    ${resolvectl} dnssec p4net off
+  '';
+
   system.activationScripts.bonus = ''
     echo "b15644912e097589=p4net" > /var/lib/zerotier-one/devicemap;
     echo "93afae59633bb8b8=kncyber" >> /var/lib/zerotier-one/devicemap;
   '';
 
   systemd.network = {
-    #networks."50-zerotier" = {
-    #  name = "p4net";
-    #  dns = [ "198.18.66.1" ];
-    #  domains = [ "p4" ];
-    #  networkConfig = {
-    #    DNSSEC = false;
-    #  };
-    #};
     wait-online.ignoredInterfaces = [ "p4net" "kncyber" ];
   };
 }
