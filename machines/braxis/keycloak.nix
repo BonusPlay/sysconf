e152bea22bc5461b20802c4e94c62d5410e8c65f
Bonus <root@bonusplay.pl>
2023-04-23 15:33:28 +0200

    braxis: enable keycloak metrics

--- machines/braxis/keycloak.nix
+++ machines/braxis/keycloak.nix
@@ -11,6 +11,7 @@
     settings = {
       hostname = "keycloak.kncyber.pl";
       proxy = "edge";
+      metrics-enabled = true;
     };
     database.passwordFile = config.age.secrets.dbPass.path;
     initialAdminPassword = "8mJjIry1j9xNxLDT9qPbRj8taRQOzEZhRehwCVwQmHk=";
