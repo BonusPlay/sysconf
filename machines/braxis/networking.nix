bc5f86af2018488733c5418f9aae453c54c691ee
Bonus <root@bonusplay.pl>
2023-05-03 15:09:08 +0200

    braxis: static p4net ip

--- machines/braxis/networking.nix
+++ machines/braxis/networking.nix
@@ -2,13 +2,29 @@
 {
   networking = {
     hostName = "braxis";
-    nameservers = [ "1.1.1.1" "8.8.8.8" ];
-    useDHCP = true;
+    nameservers = [ "198.18.66.1" ];
+    dhcpcd.enable = false;
+    useDHCP = false;
 
     firewall = {
       enable = true;
       allowedTCPPorts = [ 13337 ];
       allowedUDPPorts = [ 13337 ];
+    };
+
+    interfaces.ens18 = {
+      useDHCP = false;
+      ipv4 = {
+        addresses = [{
+          address = "198.18.68.20";
+          prefixLength = 24;
+        }];
+        routes = [{
+          address = "0.0.0.0";
+          prefixLength = 0;
+          via = "198.18.68.1";
+        }];
+      };
     };
   };
 }
