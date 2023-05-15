78f718a2544b34928d42782b63b64de035ea143e
Bonus <root@bonusplay.pl>
2023-05-16 01:05:26 +0200

    endion: disable gitea registration

--- machines/endion/gitea.nix
+++ machines/endion/gitea.nix
@@ -8,7 +8,7 @@
         enable = true;
         settings = {
           session.COOKIE_SECURE = true;
-          #service.DISABLE_REGISTRATION = true;
+          service.DISABLE_REGISTRATION = true;
           server.SSH_PORT = 2222;
         };
         rootUrl = "https://git.bonus.p4";
