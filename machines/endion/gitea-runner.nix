217224e7bcc09248ddfd87c83bbc0a87cc81545c
Bonus <root@bonusplay.pl>
2023-05-22 23:10:30 +0200

    endion: disable gitea-runner

--- machines/endion/gitea-runner.nix
+++ machines/endion/gitea-runner.nix
@@ -3,31 +3,31 @@ let
   tokenFile = "/run/giteaToken";
 in
 {
-  age.secrets.giteaRunnerLinuxToken = {
-    file = ../../secrets/gitea-runner-linux-token.age;
-    mode = "0400";
-    owner = "gitea-runner";
-  };
+  #age.secrets.giteaRunnerLinuxToken = {
+  #  file = ../../secrets/gitea-runner-linux-token.age;
+  #  mode = "0400";
+  #  owner = "gitea-runner";
+  #};
 
-  containers.gitea-runner = {
-    autoStart = true;
-    bindMounts."${tokenFile}" = {
-      hostPath = config.age.secrets.giteaRunnerLinuxToken.path;
-      isReadOnly = true;
-    };
+  #containers.gitea-runner = {
+  #  autoStart = true;
+  #  bindMounts."${tokenFile}" = {
+  #    hostPath = config.age.secrets.giteaRunnerLinuxToken.path;
+  #    isReadOnly = true;
+  #  };
 
-    config = { config, pkgs, ... }: {
-      services.gitea-actions-runner.instances.linux_amd64 = {
-        enable = true;
-        url = "https://git.bonus.p4";
-        tokenFile = tokenFile;
-        labels = [ "linux_amd64:host" ];
-      };
+  #  config = { config, pkgs, ... }: {
+  #    services.gitea-actions-runner.instances.linux_amd64 = {
+  #      enable = true;
+  #      url = "https://git.bonus.p4";
+  #      tokenFile = tokenFile;
+  #      labels = [ "linux_amd64:host" ];
+  #    };
 
-      system.stateVersion = "unstable";
-      networking.firewall = {
-        enable = true;
-      };
-    };
-  };
+  #    system.stateVersion = "unstable";
+  #    networking.firewall = {
+  #      enable = true;
+  #    };
+  #  };
+  #};
 }
