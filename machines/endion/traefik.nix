02cce7ef5e1e25043defe87986fc25ed1a55f963
Bonus <root@bonusplay.pl>
2023-05-22 23:22:00 +0200

    endion: disable p4net filter

--- machines/endion/traefik.nix
+++ machines/endion/traefik.nix
@@ -34,29 +34,23 @@
     };
     dynamicConfigOptions =
       let
-        p4netMiddleware = {
-          p4net.ipwhitelist.sourcerange = "127.0.0.1/32, 198.18.0.0/16";
-        };
         entries = [
           {
             name = "git";
             domain = "git.bonus.p4";
             kind = "http";
             port = config.services.gitea.settings.server.HTTP_PORT;
-            middlewares = [ p4netMiddleware ];
           }
         ];
         mkHttpEntry = entry: {
           routers."${entry.name}" = {
             rule = "Host(`${entry.domain}`)";
             service = entry.name;
-            middlewares = lib.flatten (map lib.attrNames entry.middlewares);
             entrypoints = [ "websecure" ];
           };
           services."${entry.name}".loadBalancer.servers = [{
             url = "http://${entry.target or "localhost"}:${toString entry.port}";
           }];
-          middlewares = lib.foldl' lib.recursiveUpdate {} entry.middlewares;
         };
         httpEntries = map mkHttpEntry entries;
         httpConfig = lib.foldl' lib.recursiveUpdate {} httpEntries;
