41027eef21d827ead65564fc181272a025a98fec
Bonus <root@bonusplay.pl>
2023-05-24 19:00:24 +0200

    kaldir: add zerotier interface blacklist

--- machines/kaldir/p4net-zerotier.nix
+++ machines/kaldir/p4net-zerotier.nix
@@ -6,9 +6,16 @@
     joinNetworks = [ "b15644912e097589" ];
   };
 
-  system.activationScripts.bonus = ''
+  system.activationScripts.bonus = let
+    local = builtins.toJSON {
+      settings = {
+        interfacePrefixBlacklist = [ "p4net-" "ve-" ];
+      };
+    };
+  in ''
     echo "b15644912e097589=p4net-khala" > /var/lib/zerotier-one/devicemap;
+    cat << EOF > /var/lib/zerotier-one/local.conf
+      ${local}
+    EOF
   '';
-
-  # TODO: configure interfacePrefixBlacklist
 }
