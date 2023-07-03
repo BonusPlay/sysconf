b421b1c5a06bac71de0ec5b0fcf3ddd8cd80374d
Bonus <root@bonusplay.pl>
2023-07-03 23:05:34 +0200

    move vms to DMZ

--- machines/braxis/networking.nix
+++ machines/braxis/networking.nix
@@ -9,20 +9,5 @@
       allowedTCPPorts = [ 13337 ];
       allowedUDPPorts = [ 13337 ];
     };
-
-    interfaces.ens18 = {
-      useDHCP = false;
-      ipv4 = {
-        addresses = [{
-          address = "10.20.31.40";
-          prefixLength = 24;
-        }];
-        routes = [{
-          address = "0.0.0.0";
-          prefixLength = 0;
-          via = "10.20.31.11";
-        }];
-      };
-    };
   };
 }
--- machines/shakuras/networking.nix
+++ machines/shakuras/networking.nix
@@ -3,30 +3,11 @@
   networking = {
     hostName = "shakuras";
     nameservers = [ "1.1.1.1" "8.8.8.8" ];
-    dhcpcd.enable = false;
-    useDHCP = false;
 
     firewall = {
       enable = true;
       allowedTCPPorts = [ 13337 ];
       allowedUDPPorts = [ 13337 ];
-    };
-
-    interfaces = {
-      ens18 = {
-        useDHCP = false;
-        ipv4 = {
-          addresses = [{
-            address = "10.20.31.31";
-            prefixLength = 24;
-          }];
-          routes = [{
-            address = "0.0.0.0";
-            prefixLength = 0;
-            via = "10.20.31.11";
-          }];
-        };
-      };
     };
   };
 }
