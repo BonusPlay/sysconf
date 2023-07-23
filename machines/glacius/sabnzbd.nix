9b908f194ad25596ffdd8156b57a93fce8742252
Bonus <root@bonusplay.pl>
2023-07-23 19:06:40 +0200

    glacius: fix ports

--- machines/glacius/sabnzbd.nix
+++ machines/glacius/sabnzbd.nix
@@ -1,13 +1,11 @@
 {
-  networking.firewall.allowedTCPPorts = [];
-
   containers.sabnzbd = {
     autoStart = true;
     privateNetwork = true;
     hostAddress = "172.28.0.1";
     localAddress = "172.28.0.11";
     forwardPorts = [{
-      containerPort = 80;
+      containerPort = 8080;
       hostPort = 8080;
       protocol = "tcp";
     }];
@@ -17,13 +15,13 @@
         "unrar"
       ];
 
-      services.sabnzbd = {
-        enable = true;
-      };
+      # TODO: this requires manual fix, as sabnzbd
+      # listens on 127.0.0.1
+      services.sabnzbd.enable = true;
 
       networking.firewall = {
         enable = true;
-        allowedTCPPorts = [ 80 ];
+        allowedTCPPorts = [ 8080 ];
       };
 
       system.stateVersion = "23.05";
