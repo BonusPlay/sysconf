82a27144c1535d7b840ff7e21fb94aed0eaf643d
Bonus <root@bonusplay.pl>
2023-09-14 23:43:40 +0200

    home: fix typo

--- home/tmux.nix
+++ home/tmux.nix
@@ -5,7 +5,7 @@
     extraConfig = ''
       set -g default-terminal "screen-256color"
       set -g renumber-windows on
-      undind-key -T copy-mode-vi v                             # Unbind old
+      unbind-key -T copy-mode-vi v                             # Unbind old
       bind-key -T copy-mode-vi 'v' send -X begin-selection     # Begin selection in copy mode.
       bind-key -T copy-mode-vi 'C-v' send -X rectangle-toggle  # Begin selection in copy mode.
       bind-key -T copy-mode-vi 'y' send -X copy-selection      # Yank selection in copy mode.
