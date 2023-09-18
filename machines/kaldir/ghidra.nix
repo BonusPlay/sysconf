1d36ba43afbb3efc06c84217dbee72266683a40e
Bonus <root@bonusplay.pl>
2023-09-18 19:38:13 +0200

    kaldir: hotfix

--- machines/kaldir/ghidra.nix
+++ machines/kaldir/ghidra.nix
@@ -6,7 +6,7 @@ in
 
   virtualisation.oci-containers.containers.ghidra = {
     image = "blacktop/ghidra:10.3.3-alpine";
-    ports = [ "${port}:${port}" ];
+    ports = [ "${toString port}:${toString port}" ];
     volumes = [ "/var/lib/ghidra:/repos" ];
     cmd = [ "server" ];
   };
