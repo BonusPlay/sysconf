61581bfab6ccedf2df237536cf76d62d85d03018
Bonus <root@bonusplay.pl>
2023-09-20 22:55:18 +0200

    glacius: remove vms storage

--- machines/glacius/nfs.nix
+++ machines/glacius/nfs.nix
@@ -6,9 +6,8 @@
       subnets = [ "100.64.0.0/10" "192.168.5.0/24" "192.168.10.0/24" ];
       line = builtins.concatStringsSep " " (builtins.map (x: x + "(rw,sync,no_subtree_check)") subnets);
     in ''
-      /storage/general  ${line}
-      /storage/backups  ${line}
-      /storage/vms      192.168.10.10(rw,async,no_subtree_check,no_root_squash)
+      /storage/general ${line}
+      /storage/backups ${line}
     '';
   };
 
