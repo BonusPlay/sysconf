ec9950569ac19bf40fd3468624d24d3423edf10d
Bonus <root@bonusplay.pl>
2023-09-24 12:59:53 +0200

    kaldir: hotfix image sha

--- machines/kaldir/ghidra.nix
+++ machines/kaldir/ghidra.nix
@@ -10,7 +10,7 @@ in
   virtualisation.oci-containers.backend = "podman";
   virtualisation.oci-containers.containers.ghidra = {
     # 10.3.3-alpine
-    image = "blacktop/ghidra@sha256:464929a50b74180213872dd740be3c44cdfeb9bcd948a9a242e5c4f8805d3cc3";
+    image = "blacktop/ghidra@sha256:a8d1f1095c3519890f093a844cf81f660a72518392511cdd72fd6faef322ff1e";
     ports = map mkPair (map toString ports);
     volumes = [ "/var/lib/ghidra:/repos" ];
     cmd = [ "server" ];
