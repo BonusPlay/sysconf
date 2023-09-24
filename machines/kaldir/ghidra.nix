0246544986d270847eb3ede5bda95be1e3f39a48
Bonus <root@bonusplay.pl>
2023-09-24 12:55:52 +0200

    kaldir: fix issues with ghidra

--- machines/kaldir/ghidra.nix
+++ machines/kaldir/ghidra.nix
@@ -1,18 +1,21 @@
+{ pkgs, ... }:
 let
   ports = [ 13100 13101 13102 ];
   mkPair = port: "${port}:${port}";
+  ipcmd = "${pkgs.dig}/bin/dig +short ghidra.bonusplay.pl | tail -1 | tr -d '\n'";
 in
 {
   networking.firewall.allowedTCPPorts = ports;
 
-  virtualisation.oci-containers.backend = "docker";
+  virtualisation.oci-containers.backend = "podman";
   virtualisation.oci-containers.containers.ghidra = {
-    image = "blacktop/ghidra:10.3.3-alpine";
+    # 10.3.3-alpine
+    image = "blacktop/ghidra@sha256:464929a50b74180213872dd740be3c44cdfeb9bcd948a9a242e5c4f8805d3cc3";
     ports = map mkPair (map toString ports);
     volumes = [ "/var/lib/ghidra:/repos" ];
     cmd = [ "server" ];
     environment = {
-      GHIDRA_IP = "0.0.0.0";
+      GHIDRA_IP = "$(${ipcmd})";
       GHIDRA_USERS = "bonus";
     };
   };
