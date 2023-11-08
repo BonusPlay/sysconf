f3359381ced74bb630b5ff48e336beb2e956d10e
Bonus <root@bonusplay.pl>
2023-11-08 13:10:04 +0100

    modules: fix traefik config

--- modules/traefik.nix
+++ modules/traefik.nix
@@ -143,9 +143,8 @@ in
             }];
             middlewares = dummyMiddleware // (lib.foldl' lib.recursiveUpdate {} entry.middlewares);
           };
-          middlewares = lib.foldl' lib.recursiveUpdate {} (map (entry: lib.foldl' lib.recursiveUpdate {} entry.middlewares) cfg.entries);
           httpEntries = map mkHttpEntry cfg.entries;
-          httpConfig = (lib.foldl' lib.recursiveUpdate {} httpEntries) // middlewares;
+          httpConfig = lib.foldl' lib.recursiveUpdate {} httpEntries;
 
           mkTlsEntry = domain: {
             certFile = "/var/lib/acme/${domain}/cert.pem";
