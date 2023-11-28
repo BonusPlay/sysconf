1191cf9de029f60a4cce3292163ddccc08426c15
Bonus <root@bonusplay.pl>
2023-11-28 19:03:05 +0100

    moria: move plex to host network

--- machines/moria/plex.nix
+++ machines/moria/plex.nix
@@ -5,16 +5,12 @@
       name = "plex";
       domain = "plex.bonusplay.pl";
       port = 32400;
-      target = config.containers.plex.localAddress;
       entrypoints = [ "warps" ];
     }
   ];
 
   containers.plex = {
     autoStart = true;
-    privateNetwork = true;
-    hostAddress = "172.28.1.1";
-    localAddress = "172.28.1.2";
     bindMounts = {
       "/storage" = {
         hostPath = "/storage";
