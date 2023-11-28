650d1af0e8c80254dbc1b9bfc40263ee44cbd267
Bonus <root@bonusplay.pl>
2023-11-28 19:17:53 +0100

    Revert "moria: move plex to host network"
    This reverts commit 1191cf9de029f60a4cce3292163ddccc08426c15.

--- machines/moria/plex.nix
+++ machines/moria/plex.nix
@@ -5,12 +5,16 @@
       name = "plex";
       domain = "plex.bonusplay.pl";
       port = 32400;
+      target = config.containers.plex.localAddress;
       entrypoints = [ "warps" ];
     }
   ];
 
   containers.plex = {
     autoStart = true;
+    privateNetwork = true;
+    hostAddress = "172.28.1.1";
+    localAddress = "172.28.1.2";
     bindMounts = {
       "/storage" = {
         hostPath = "/storage";
