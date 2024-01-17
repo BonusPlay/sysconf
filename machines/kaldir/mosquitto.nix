c68bffdae88dd1a47384e4921b8917c6c96c92b2
Bonus <root@bonusplay.pl>
2024-01-18 00:58:51 +0100

    kaldir: more mosquitto users

--- machines/kaldir/mosquitto.nix
+++ machines/kaldir/mosquitto.nix
@@ -17,6 +17,14 @@ in
               acl = [ "readwrite esp32/#" ];
               hashedPassword = "$7$101$/JRnReEzuZpPuDIO$H9tGTGSv8y6+l8UJsHuYjlJxxeDqfzxM2MeXBavoYigJ57UASmwnpY8s88RaSBBGyenZy8BGsyRtdsFXGICayA==";
             };
+            "influx" = {
+              acl = [ "read #" ];
+              hashedPassword = "$7$101$xYbNIGLUm0/j7aN/$zjfObOh7NqO1LRQET1dNxj2F1ft20W5beb7cpT35Q75MP1ObKgizlQ1yJITBFNN40qQGx8z4hD7jOblwWK8k6g==";
+            };
+            "bonus" = {
+              acl = [ "readwrite #" ];
+              hashedPassword = "$7$101$ZC+JXpJe8n4mCjDz$rYZ+t/zy7Zc03qBNGoP+q02c+DqwXJXHEzKNP6nuMt9DmFY9UE+zCGb3nkHxxy4la54xVe4/9HMTTZLVY1ZbPA==";
+            };
           };
         }];
       };
