e130cc3cdfd31b42e0c27768c24db5e78456c607
Bonus <root@bonusplay.pl>
2024-01-18 01:33:13 +0100

    kaldir: put telegraf-mqtt in container

--- machines/kaldir/telegraf-mqtt.nix
+++ machines/kaldir/telegraf-mqtt.nix
@@ -1,34 +1,50 @@
 { config, lib, ... }:
+let
+  telegraf-mqtt-env = "/run/telegraf-mqtt-env";
+in
 {
-  age.secrets.telegraf-mqtt = {
+  age.secrets.telegraf-mqtt-env = {
     file = ../../secrets/telegraf-mqtt-env.age;
     mode = "0400";
     owner = "telegraf";
   };
 
-  services.telegraf = {
-    enable = true;
-    environmentFiles = [ config.age.secrets.telegraf-mqtt.path ];
-    extraConfig = {
-      inputs = {
-        mqtt_consumer = {
-          servers = [ "tcp://mqtt.bonusplay.pl:8883" ];
-          topics = [ "sensors/#" ];
-          username = "influx";
-          password = "$MQTT_PASSWORD";
-          data_format = "value";
-          data_type = "float";
+  containers.telegraf-mqtt = {
+    autoStart = true;
+    bindMounts.telegraf-mqtt-env = {
+      hostPath = config.age.secrets.telegraf-mqtt-env.path;
+      mountPoint = telegraf-mqtt-env;
+      isReadOnly = true;
+    };
+
+    config = { config, ... }: {
+      services.telegraf = {
+        enable = true;
+        environmentFiles = [ telegraf-mqtt-env ];
+        extraConfig = {
+          inputs = {
+            mqtt_consumer = {
+              servers = [ "tcp://mqtt.bonusplay.pl:8883" ];
+              topics = [ "sensors/#" ];
+              username = "influx";
+              password = "$MQTT_PASSWORD";
+              data_format = "value";
+              data_type = "float";
+            };
+          };
+          outputs = {
+            influxdb_v2 = {
+              urls = [ "https://influx.mlwr.dev" ];
+              token = "$INFLUX_TOKEN";
+              organization = "khala";
+              bucket = "sensors";
+              content_encoding = "gzip";
+            };
+          };
         };
       };
-      outputs = {
-        influxdb_v2 = {
-          urls = [ "https://influx.mlwr.dev" ];
-          token = "$INFLUX_TOKEN";
-          organization = "khala";
-          bucket = "sensors";
-          content_encoding = "gzip";
-        };
-      };
+      
+      system.stateVersion = "23.11";
     };
   };
 }
