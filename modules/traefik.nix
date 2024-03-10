167db69de579517dbc3faa9d29183423d83f9d21
Bonus <root@bonusplay.pl>
2024-03-10 01:08:44 +0100

    modules: enable traefik access log

--- modules/traefik.nix
+++ modules/traefik.nix
@@ -122,6 +122,7 @@ in
         api = {
           dashboard = true;
         };
+        accessLog = {};
         entryPoints = publicEntrypoint // warpEntrypoint;
       };
       dynamicConfigOptions =
