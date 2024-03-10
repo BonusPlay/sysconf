4ddbc6b82e63bb5221f816c9d940c2abf197224b
Bonus <root@bonusplay.pl>
2024-03-10 01:24:38 +0100

    flake: update

--- flake.nix
+++ flake.nix
@@ -2,7 +2,6 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs-ghidra.url = "github:roblabla/nixpkgs?rev=25ec5c2344a9b64616701f57e5da216c33d7765f";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     agenix = {
