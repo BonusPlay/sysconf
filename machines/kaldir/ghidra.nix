e3608bfdf32ad8bbb840e8d5bd25015443789f6e
Bonus <root@bonusplay.pl>
2024-03-13 18:15:54 +0100

    kaldir: update ghidra-server

--- machines/kaldir/ghidra.nix
+++ machines/kaldir/ghidra.nix
@@ -8,8 +8,8 @@ in
 
   virtualisation.oci-containers.backend = "podman";
   virtualisation.oci-containers.containers.ghidra = {
-    # 10.3.3-alpine
-    image = "blacktop/ghidra@sha256:a8d1f1095c3519890f093a844cf81f660a72518392511cdd72fd6faef322ff1e";
+    # 11.0.1-alpine
+    image = "blacktop/ghidra@sha256:f93de31b54d6ba98fb31c247ce534a342ab587db2340943c4ad899d808469a46";
     ports = map mkPair (map toString ports);
     volumes = [ "/var/lib/ghidra:/repos" ];
     cmd = [ "server" ];
