79a2c2e09284bdfe086b671627f5e5dd8226c5bb
Bonus <root@bonusplay.pl>
2024-07-13 20:52:49 +0200

    home: add ssh config

--- home/ssh.nix
+++ home/ssh.nix
@@ -4,5 +4,19 @@
     compression = true;
     hashKnownHosts = false;
     serverAliveInterval = 60;
+    matchBlocks = {
+      "crabdev" = {
+        hostname = "crabdev-bios.warp.lan";
+        user = "a";
+        identitiesOnly = true;
+        identityFile = [ "/home/bonus/.ssh/id_crabdev" ];
+      };
+      "ctf" = {
+        hostname = "ctf.warp.lan";
+        user = "xakep";
+        identitiesOnly = true;
+        identityFile = [ "/home/bonus/.ssh/id_ctf" ];
+      };
+    };
   };
 }
