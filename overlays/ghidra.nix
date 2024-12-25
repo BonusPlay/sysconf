e9408daeb1def23c43880b10c59f104df71fcf2e
Bonus <root@bonusplay.pl>
2024-12-25 20:23:01 +0100

    hotfix: fix ghidra overlay

--- overlays/ghidra.nix
+++ overlays/ghidra.nix
@@ -1,9 +1,13 @@
 { nixpkgs-unstable }:
-final: prev: {
-  ghidra = nixpkgs-unstable.legacyPackages.${prev.system}.ghidra;
-  ghidra-extensions = {
-    arcompact = prev.callPackage ../pkgs/ghidra-arcompact.nix {};
-    ctrlp = prev.callPackage ../pkgs/ghidra-ctrlp.nix {};
-    wasm = prev.callPackage ../pkgs/ghidra-wasm.nix {};
-  } // nixpkgs-unstable.legacyPackages.${prev.system}.ghidra-extensions;
-}
+final: prev:
+  let
+    pkgs = nixpkgs-unstable.legacyPackages.${prev.system};
+  in
+  {
+    ghidra = pkgs.ghidra;
+    ghidra-extensions = {
+      arcompact = pkgs.callPackage ../pkgs/ghidra-arcompact.nix {};
+      ctrlp = pkgs.callPackage ../pkgs/ghidra-ctrlp.nix {};
+      wasm = pkgs.callPackage ../pkgs/ghidra-wasm.nix {};
+    } // pkgs.ghidra-extensions;
+  }
