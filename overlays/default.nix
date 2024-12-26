e19f43c273ab52efd027fc00748ce984ed061c05
Bonus <root@bonusplay.pl>
2024-12-26 03:03:13 +0100

    hehe

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,4 +3,5 @@
   (import ./agenix.nix { inherit agenix; })
   (import ./ghidra.nix { inherit nixpkgs-unstable; })
   (import ./mautrix.nix)
+  (final: prev: { onlyoffice-documentserver = prev.callPackage ../pkgs/hax.nix {}; })
 ]
