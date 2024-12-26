5f9e52999ae1f3928496300e02f7b40497b70902
Bonus <root@bonusplay.pl>
2024-12-26 03:03:51 +0100

    artanis: add wired interface

--- machines/artanis/networking.nix
+++ machines/artanis/networking.nix
@@ -8,9 +8,15 @@
     mode = "600";
   };
 
-  systemd.network.networks."10-wifi" = {
-    matchConfig.Name = "wlp166s0";
-    networkConfig.DHCP = "yes";
+  systemd.network.networks = {
+    "10-wifi" = {
+      matchConfig.Name = "wlp166s0";
+      networkConfig.DHCP = "yes";
+    };
+    "11-wired" = {
+      matchConfig.Name = "enp0s13f0u3";
+      networkConfig.DHCP = "yes";
+    };
   };
 
   networking = {
