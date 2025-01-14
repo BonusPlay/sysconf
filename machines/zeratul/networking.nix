272e3183dc92fae125cff3ef5051de466a216f38
Bonus <root@bonusplay.pl>
2025-01-14 01:22:18 +0100

    zeratul: fix networking

--- machines/zeratul/networking.nix
+++ machines/zeratul/networking.nix
@@ -1,7 +1,15 @@
 { config, pkgs, ... }:
 {
-  systemd.network.enable = true;
-  networking.dhcpcd.enable = false;
+  systemd.network.networks = {
+    "10-sfp" = {
+      matchConfig.Name = "enp3s0f0";
+      networkConfig.DHCP = "yes";
+    };
+    "12-eth" = {
+      matchConfig.Name = "enp0s31f6";
+      networkConfig.DHCP = "yes";
+    };
+  };
 
   networking = {
     hostName = "zeratul";
