578adb720e5af52086324e970379d4e8d633a277
Bonus <root@bonusplay.pl>
2025-02-01 00:44:03 +0100

    kaldir: uptime-kuma cleanup

--- machines/kaldir/uptime.nix
+++ machines/kaldir/uptime.nix
@@ -1,13 +1,5 @@
 { config, lib, ... }:
 {
-  custom.caddy.entries = [
-    {
-      entrypoints = [ "100.98.118.66" ];
-      domain = "uptime.warp.lan";
-      port = lib.strings.toInt config.services.uptime-kuma.settings.PORT;
-    }
-  ];
-
   services.uptime-kuma = {
     enable = true;
     appriseSupport = true;
@@ -17,10 +9,11 @@
     };
   };
 
-  security.pki.certificateFiles = [ ../../files/warp-net-root.crt ];
-
-  networking.firewall = {
-    enable = true;
-    allowedTCPPorts = [ (lib.strings.toInt config.services.uptime-kuma.settings.PORT) ];
-  };
+  custom.caddy.entries = [
+    {
+      entrypoints = [ "100.98.118.66" ];
+      domain = "uptime.warp.lan";
+      port = lib.strings.toInt config.services.uptime-kuma.settings.PORT;
+    }
+  ];
 }
