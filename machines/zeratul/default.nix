717141c8acd2cc2ae40f8e193762bdf73ec64aa6
Bonus <root@bonusplay.pl>
2025-03-15 11:04:33 +0100

    zeratul: try to fix kvm

--- machines/zeratul/default.nix
+++ machines/zeratul/default.nix
@@ -15,6 +15,11 @@
       systemd-boot.configurationLimit = 5;
       timeout = 5;
     };
+    initrd.kernelModules = [ "amdgpu" ];
+    kernelParams = [
+      "video=card2-DP-5:1920x1080@60" # DP monitor
+      "video=card1-HDMI-A-1:1920x1080@60" # kvm
+    ];
     kernelModules = [ "lkrg" ];
     bootspec.enable = true;
     lanzaboote = {
