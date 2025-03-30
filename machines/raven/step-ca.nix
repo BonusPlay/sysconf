6f476e69f613e61b44f2b230c9a1c9b5669b4486
Bonus <root@bonusplay.pl>
2025-03-30 01:05:39 +0100

    raven: add JWK provider

--- machines/raven/step-ca.nix
+++ machines/raven/step-ca.nix
@@ -30,16 +30,36 @@
       crt = config.age.secrets.intermediate-crt.path;
       key = config.age.secrets.intermediate-key.path;
       dnsNames = [ "pki.warp.lan" ];
-      authority.provisioners = [
-        {
-          type = "ACME";
-          name = "warp";
-          forceCN = true;
-          challenges = ["http-01" "tls-alpn-01"];
-          disableSmallstepExtensions = true;
-          options.x509.templateFile = "/etc/smallstep/leaf.tpl";
-        }
-      ];
+      authority = {
+        claims = {
+          minTLSCertDuration = "5m";
+          maxTLSCertDuration = "8760h";
+          defaultTLSCertDuration = "48h";
+        };
+        provisioners = [
+          {
+            type = "ACME";
+            name = "warp";
+            forceCN = true;
+            challenges = ["http-01" "tls-alpn-01"];
+            disableSmallstepExtensions = true;
+            options.x509.templateFile = "/etc/smallstep/leaf.tpl";
+          }
+          {
+            type = "JWK";
+            name = "jwk";
+            key = {
+              use = "sig";
+              kty = "OKP";
+              kid = "y2ozd5b1_1FTAQFaXJy50bscW3RSx3MYDokIbzuKbcU";
+              crv = "Ed25519";
+              alg = "EdDSA";
+              x = "Buy89wJj9eWooDe4bOfveyEaMhTbgEUdp78mt8tWAJQ";
+            };
+            encryptedKey = "eyJhbGciOiJQQkVTMi1IUzUxMitBMjU2S1ciLCJlbmMiOiJBMjU2R0NNIiwicDJjIjo2MDAwMDAsInAycyI6IlBqMjJydjNXZkJvNk1PZlU0NzZXUncifQ.WT20cO4XTQThjnw9U62hMKlXCQqq3-v9dID_HqrpQM-xcHT3K1t-Vg.o7O_cI8--ZouXLzk.yCG8fd4EqR_hQ7gz0y9NtEKI-0LIi8HDBMSTFlVqQJ5rbwT2oowH4FfONQFn0dtBPpgqm-LVB8NxyumzYlxkur4mL8pxIYOv70RDCeyV4IxSy--pw-xOaiidWr1Tpzgbm2YwgQXZuYMMHMvb--N8gs2oMCts2PmTrkzemuH6TuJBoThP4jsR9EORGRS1k98IMPIDz0v9jeQpJ_0K3o7TBm_eH95q4URsriix24qMLKWgzIUCCuqXmkHM5i_U6O3bt9u0JUVjTQF7RtCI7ayh.UME4ogom43Ipk4ZH4tEQdg";
+          }
+        ];
+      };
       db = {
         type = "badger";
         dataSource = "/var/lib/step-ca/db";
