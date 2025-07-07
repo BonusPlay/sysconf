1f2131082136e5d8137c6c971ec4bc464f5b3160
Bonus <root@bonusplay.pl>
2025-07-07 20:25:56 +0200

    kaldir: attempt to fix container networks

--- machines/kaldir/networking.nix
+++ machines/kaldir/networking.nix
@@ -13,11 +13,17 @@
     };
   };
 
-  systemd.network.networks."10-wired" = {
-    matchConfig.Name = "enp0s3";
-    networkConfig = {
-      Address = "10.0.0.131/24";
-      Gateway = "10.0.0.1";
+  systemd.network.networks = {
+    "10-wired" = {
+      matchConfig.Name = "enp0s3";
+      networkConfig = {
+        Address = "10.0.0.131/24";
+        Gateway = "10.0.0.1";
+      };
+    };
+    "40-containers" = {
+      matchConfig.Name = "ve-*";
+      linkConfig.Unmanaged = true;
     };
   };
 }
