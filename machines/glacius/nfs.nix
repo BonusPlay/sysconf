6f5ecb7e35049237af97be0d9d7dad95a8179ada
Bonus <root@bonusplay.pl>
2025-07-24 21:32:55 +0200

    glacius: add plex to nfs

--- machines/glacius/nfs.nix
+++ machines/glacius/nfs.nix
@@ -4,10 +4,11 @@
     enable = true;
     exports = let
       moria = "192.168.121.43(rw,sync,no_subtree_check,all_squash,anonuid=1250,anongid=1250)";
+      plex = "192.168.126.45(ro,sync,no_subtree_check,all_squash,anonuid=1250,anongid=1250)";
     in ''
-      /storage/music     ${moria}
-      /storage/movies    ${moria}
-      /storage/tvshows   ${moria}
+      /storage/music     ${moria} ${plex}
+      /storage/movies    ${moria} ${plex}
+      /storage/tvshows   ${moria} ${plex}
       /storage/backups   192.168.10.200(rw,sync,no_subtree_check,all_squash,anonuid=1249,anongid=1249)
     '';
   };
