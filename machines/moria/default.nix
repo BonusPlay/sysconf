36cc75a36dfeaef8cb3a7fd37446f9a410b0399e
Bonus <root@bonusplay.pl>
2025-07-24 23:02:53 +0200

    moria: add docker sock

--- machines/moria/default.nix
+++ machines/moria/default.nix
@@ -31,6 +31,7 @@
     podman = {
       enable = true;
       dockerCompat = true;
+      dockerSocket.enable = true;
       defaultNetwork.settings.dns_enabled = true;
     };
   };
--- machines/plex/default.nix
+++ machines/plex/default.nix
@@ -30,6 +30,7 @@
     podman = {
       enable = true;
       dockerCompat = true;
+      dockerSocket.enable = true;
       defaultNetwork.settings.dns_enabled = true;
     };
   };
