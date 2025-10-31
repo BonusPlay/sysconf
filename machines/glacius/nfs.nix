6113060436a106e7770d2ced1b7b7c185d2cc75c
Bonus <root@bonusplay.pl>
2025-10-31 17:59:40 +0100

    glacius: re-add warez

--- machines/glacius/nfs.nix
+++ machines/glacius/nfs.nix
@@ -5,11 +5,13 @@
     exports = let
       moria = "192.168.121.43(rw,sync,no_subtree_check,all_squash,anonuid=1250,anongid=1250)";
       plex = "192.168.126.45(ro,sync,no_subtree_check,all_squash,anonuid=1250,anongid=1250)";
+      zeratul = "192.168.10.200(rw,sync,no_subtree_check,all_squash,anonuid=1249,anongid=1249)";
     in ''
       /storage/music     ${moria} ${plex}
       /storage/movies    ${moria} ${plex}
       /storage/tvshows   ${moria} ${plex}
-      /storage/backups   192.168.10.200(rw,sync,no_subtree_check,all_squash,anonuid=1249,anongid=1249)
+      /storage/backups   ${zeratul}
+      /storage/warez     ${zeratul}
     '';
   };
 
