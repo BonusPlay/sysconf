c156d10fa9bed9e6310e3dc1e40026639908c3bd
Bonus <root@bonusplay.pl>
2025-11-14 01:21:24 +0100

    bunker: bump nextcloud

--- machines/bunker/nextcloud.nix
+++ machines/bunker/nextcloud.nix
@@ -2,7 +2,7 @@
 {
   services.nextcloud = {
     enable = true;
-    package = pkgs.nextcloud32;
+    package = pkgs.nextcloud33;
     hostName = "nextcloud.bonus.re";
     https = true;
     configureRedis = true;
