26f3a65235197f077645aa852e4efa47c2f28846
Bonus <root@bonusplay.pl>
2025-12-15 01:52:47 +0100

    kaldir: remove litellm

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,6 +3,5 @@
   (import ./agenix.nix { inherit agenix; })
   (import ./beszel.nix { inherit nixpkgs-unstable; })
   (import ./ghidra.nix { inherit nixpkgs-unstable; })
-  (import ./open-webui.nix)
   (import ./web-utils.nix { inherit web-utils; })
 ]
