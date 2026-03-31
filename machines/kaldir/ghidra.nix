a2dfb1724ee3cc671057a13da6c5948168b83375
Bonus <root@bonusplay.pl>
2026-03-31 20:08:47 +0200

    kaldir: update ghidra

--- machines/kaldir/ghidra.nix
+++ machines/kaldir/ghidra.nix
@@ -6,14 +6,13 @@ in
 {
   networking.firewall.allowedTCPPorts = ports;
   virtualisation.oci-containers.containers.ghidra = {
-    # 11.2.1-alpine ARM!!!
-    image = "blacktop/ghidra@sha256:4a00f0a8447536800a68df6fbe2984b721617ad0bfa79c14a0347f8624b98913";
+    # 12.0.4-alpine
+    image = "blacktop/ghidra@sha256:a5f386ce5daa32a1a601753555fdd846e75b99d8b68b72d449ac615a4eb5ce44";
     ports = map mkPair (map toString ports);
     volumes = [ "/var/lib/ghidra:/repos" ];
     cmd = [ "server" ];
     environment = {
-      # TODO: somehow avoid hardcoding IPs (this does need to be a public IP)
-      GHIDRA_IP = "130.162.49.48";
+      GHIDRA_IP = "ghidra.bonusplay.pl";
       GHIDRA_USERS = "bonus";
     };
   };
