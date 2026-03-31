f145c81c5991006a076d67cf87a6831294f4d188
Bonus <root@bonusplay.pl>
2026-03-31 20:25:38 +0200

    nexus: update homebox

--- machines/nexus/homebox.nix
+++ machines/nexus/homebox.nix
@@ -1,11 +1,14 @@
 {
-  services.homebox.enable = true;
-  # TODO: remove after overlay
-  services.homebox.settings = {
-    HBOX_DATABASE_DRIVER = "sqlite3";
-    HBOX_DATABASE_SQLITE_PATH = "/var/lib/homebox/data/homebox.db?_pragma=busy_timeout=999&_pragma=journal_mode=WAL&_fk=1";
-    HBOX_OPTIONS_ALLOW_REGISTRATION = "true";
-    HBOX_OPTIONS_CHECK_GITHUB_RELEASE = "false";
+  services.homebox = {
+    enable = true;
+    settings = {
+      HBOX_MODE = "production";
+      HBOX_DATABASE_DRIVER = "sqlite3";
+      HBOX_DATABASE_SQLITE_PATH = "/var/lib/homebox/data/homebox.db?_pragma=busy_timeout=999&_pragma=journal_mode=WAL&_fk=1";
+      HBOX_OPTIONS_ALLOW_REGISTRATION = "false";
+      HBOX_OPTIONS_CHECK_GITHUB_RELEASE = "false";
+      HBOX_OPTIONS_TRUST_PROXY= "true";
+    };
   };
   networking.firewall.allowedTCPPorts = [ 7745 ];
 }
